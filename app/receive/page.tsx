1c19367cb4b916565e13511fbe93cc1c849a41c1
ufraaan <ufraansiddiqui30@gmail.com>
2026-02-04 20:56:13 +0530

    wrap usesearchparams in suspense boundary

--- app/receive/page.tsx
+++ app/receive/page.tsx
@@ -1,6 +1,15 @@
+import { Suspense } from "react";
 import { ReceivePanel } from "@/components/transfer/ReceivePanel";
 
+function ReceivePanelFallback() {
+  return <div>Loading...</div>;
+}
+
 export default function ReceivePage() {
-  return <ReceivePanel />;
+  return (
+    <Suspense fallback={<ReceivePanelFallback />}>
+      <ReceivePanel />
+    </Suspense>
+  );
 }
 
